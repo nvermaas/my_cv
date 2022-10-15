f7c09c035766acc1229d0505f2d46c8c8a81fdf0
Nico Vermaas <vermaas@astron.nl>
2022-10-15 15:10:31 +0200

    adding some empty pages

--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -5,6 +5,9 @@ import WelcomeCard from '../components/WelcomeCard'
 export default function SkillsPage() {
 
     return (
-        <WelcomeCard/>
+        <div>
+            <h2>Skills</h2>
+            <WelcomeCard/>
+        </div>
     );
 }
